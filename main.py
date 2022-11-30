6a143c33706edbdcaa6d296013bb3eea10171ee6
Maksat <Maksatkgz6917@gmail.com>
2022-11-30 15:52:12 +0600

    main commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index_main.html')
+
+
+@app.route('/portfolio')
+def port():
+    return render_template('portfolio.html')
+
+
+@app.route('/sv')
+def sv_one():
+    style = 'style.scc'
+    score = 0
+    return render_template('index.html', style=style, sc=score)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
